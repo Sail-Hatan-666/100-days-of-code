e96c0216a457f0cd1381a9f21aee2912b56735d9
Ryan Webster <97265322+Sail-Hatan-666@users.noreply.github.com>
2025-12-18 16:31:12 -0800

    Start day16 Challenge

---
+++ Day16/main.py
@@ -0,0 +1,18 @@
+# import another_module
+# print(another_module.another_variable)
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+print(timmy)
+timmy.shape("turtle")
+timmy.forward(300)
+timmy.left(90)
+timmy.forward(300)
+timmy.left(90)
+timmy.forward(300)
+timmy.left(90)
+timmy.forward(300)
+
+
+my_screen = Screen()
+my_screen.exitonclick()
